4694cc336dc8dcd51e98447b321b02fd7625e852
Mayuri Yedlapalli <mayuriyedlapalli@Mayuris-MacBook-Pro.local>
2026-02-06 14:35:41 +0100

    first commit

---
+++ data/data.js
@@ -0,0 +1,20 @@
+export const galaxy = [
+    {
+        "type": "Main-Sequence Star",
+        "description": "Stars that fuse hydrogen into helium in their cores. Most common type of star.",
+        "examples": [
+            {
+                "name": "Sun",
+                "spectralType": "G-type",
+                "distanceLightYears": 0,
+                "color": "Yellow"
+            },
+            {
+                "name": "Sirius",
+                "spectralType": "A-type",
+                "distanceLightYears": 8.6,
+                "color": "White"
+            }
+        ]
+    }
+]
