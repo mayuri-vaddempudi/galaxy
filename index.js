e01846370b3416b0c6f89851ed34acc3918492df
Liting <janetli82@gmail.com>
2026-02-10 13:34:19 +0100

    adding sidebar

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 import express from "express"
+import express from "express"
+import * as path from "path"
+import "dotenv/config"
+
+const app = express()
+const port = process.env.PORT
+const __dirname = path.resolve()
+
+app.set("view engine",'ejs')
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+   res.render(path.join(__dirname, "/views/pages/index.ejs"))
+})
+
+app.listen(port, ()=>{
+  console.log(`Server is running on ${port}`);
+  
+})
