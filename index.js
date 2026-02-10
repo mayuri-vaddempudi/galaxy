e93e6d35c67e780a193b8786c1ab497fac785c1b
Mayuri Yedlapalli <mayuriyedlapalli@Mayuris-MacBook-Pro.local>
2026-02-10 10:22:56 +0100

    server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import express from "express"
+import path from "path"
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 4000
+const __dirname = path.resolve()
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.render("pages/index")  // No variables needed
+})
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
